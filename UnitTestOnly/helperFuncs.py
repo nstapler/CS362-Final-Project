156e857ff9bc6a20962f163a456fc0ff85210445
nstapler <staplern@bu.edu>
2020-03-06 13:22:10 -0800

    Create random input generation for tests

---
+++ UnitTestOnly/helperFuncs.py
@@ -0,0 +1,27 @@
+import random
+
+
+def prepTestInputs(testSelf):
+    random.seed()
+    # First set
+    testSelf.zeroCase = 0
+    testSelf.posInt = random.randint(1, 1000)  # 1-1000
+    testSelf.posFloat = (random.random() * random.randint(1, 999)) + 1  # 1-1000
+    testSelf.negInt = -1 * random.randint(1, 1000)  # -(1-1000)
+    testSelf.negFloat = -1 * (random.random() * random.randint(1, 999)) + 1  # -(1-1000)
+    testSelf.invalidChar = chr(random.randint(32, 126))  # ' ' to ~
+    testSelf.hugeNumber = random.randint(1000, 10000) * random.randint(1000, 10000) * random.randint(1000, 10000)  #
+    # 1mill to 1 bill
+    testSelf.fraction = 1 / (testSelf.posInt + 1)  # 1/2 to 1/1001
+    testSelf.negFraction = 1 / (testSelf.negFloat + 1)  # -(1/2 to 1/1001)
+
+    # Second Set
+    testSelf.posInt2 = random.randint(1, 1000)  # 1-1000
+    testSelf.posFloat2 = (random.random() * random.randint(1, 999)) + 1  # 1-1000
+    testSelf.negInt2 = -1 * random.randint(1, 1000)  # -(1-1000)
+    testSelf.negFloat2 = -1 * (random.random() * random.randint(1, 999)) + 1  # -(1-1000)
+    testSelf.invalidChar2 = chr(random.randint(32, 126))  # ' ' to ~
+    testSelf.hugeNumber2 = random.randint(1000, 10000) * random.randint(1000, 10000) * random.randint(1000, 10000)  #
+    # 1mill to 1 bill
+    testSelf.fraction2 = 1 / (testSelf.posInt2 + 1)  # 1/2 to 1/1001
+    testSelf.negFraction2 = 1 / (testSelf.negFloat2 + 1)  # -(1/2 to 1/1001)
--- UnitTestOnly/nick_test_calculator.py
+++ UnitTestOnly/nick_test_calculator.py
@@ -1,19 +1,25 @@
 import unittest
+
 import UnitTestOnly.nick_calculator as nick_calc
+
+import UnitTestOnly.helperFuncs as helper
 
 
 class TestMultiplication(unittest.TestCase):
     def testMultiply(self):
+        helper.prepTestInputs(self)
         self.assertNotEqual(None, nick_calc.multiplication())
 
 
 class TestFactorial(unittest.TestCase):
     def testFactorial(self):
+        helper.prepTestInputs(self)
         self.assertNotEqual(None, nick_calc.factorial())
 
 
 class TestAbsolute(unittest.TestCase):
     def testAbsolute(self):
+        helper.prepTestInputs(self)
         self.assertNotEqual(None, nick_calc.absolute())
 
 
