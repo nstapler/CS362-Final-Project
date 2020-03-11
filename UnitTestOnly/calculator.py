29226e231f5614b48d91c57af6b2d001888b926f
nstapler <staplern@bu.edu>
2020-03-10 22:26:20 -0700

    create the main functions/files

---
+++ UnitTestOnly/calculator.py
@@ -0,0 +1,34 @@
+def multiplication(val, val2):
+    return None
+
+
+def factorial(val):
+    return None
+
+
+def absolute(val):
+    return None
+
+
+def sqrRt(val):
+    return None
+
+
+def sin(val):
+    return None
+
+
+def cosine(val):
+    return None
+
+
+def division(val1, val2):
+    return None
+
+
+def square(val):
+    return None
+
+
+def fraction(val):
+    return None
